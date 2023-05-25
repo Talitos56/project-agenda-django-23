49c1eb8fdfaab772778df87b5a86190d188acbd1
Talles Correa <talles_correa@hotmail.com>
2023-05-25 13:41:14 -0300

    created category

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from contact.models import Contact
+from contact.models import Category, Contact
 
 # Register your models here.
 
@@ -15,3 +15,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
