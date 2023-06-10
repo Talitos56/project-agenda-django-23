59768e5ac7623a7d3d6e5e596352e40c5ff87d55
Talles Correa <talles_correa@hotmail.com>
2023-06-10 19:48:01 -0300

    messages

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
@@ -14,6 +15,8 @@ def register(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário registrado.')
+            return redirect('contact:index')
 
     return render(
         request,
