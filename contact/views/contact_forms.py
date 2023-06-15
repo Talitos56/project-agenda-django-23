729f6e274e0821a8a9da354dbfffa8417218939b
Talles Correa <talles_correa@hotmail.com>
2023-06-15 13:51:13 -0300

    request.user

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -17,7 +17,9 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
 
         return render(
@@ -40,7 +42,9 @@ def create(request):
 
 @login_required(login_url='contact:login')
 def update(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user
+    )
     form_action = reverse('contact:update', args=(contact_id,))
     if request.method == 'POST':
         form = forms.ContactForm(request.POST, request.FILES, instance=contact)
@@ -73,7 +77,9 @@ def update(request, contact_id):
 
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user
+    )
     confirmation = request.POST.get('confirmation', 'no')
 
     if confirmation == 'yes':
